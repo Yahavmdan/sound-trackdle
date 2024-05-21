aa7a1398c7bdc683a4b320f998a36ace7aa205b6
yahavdan <yahavmdan@gmail.com>
2024-05-21 17:05:36 +0300

    added auth

--- src/app/shared/services/file.service.ts
+++ src/app/shared/services/file.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
 import { File } from "../types/file.type";
 import { BaseService } from "./base.service";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
